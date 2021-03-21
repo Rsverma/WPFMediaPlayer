e8bdbf3da8905b4990386186916bd69e72287860
Ramesh Verma <rsverma333@gmail.com>
2021-03-21 11:09:08 +0530

    Play pause button and basic time labels added

--- WPFMediaPlayer/MainWindow.xaml.cs
+++ WPFMediaPlayer/MainWindow.xaml.cs
@@ -10,96 +10,115 @@ namespace WPFMediaPlayer
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : HandyControl.Controls.Window
     {
-		private bool mediaPlayerIsPlaying = false;
-		private bool userIsDraggingSlider = false;
+        private const string TIME_FORMAT = @"hh\:mm\:ss";
+        private bool _isplaying = false;
+        private bool _isNewFileOpened = false;
+        private TimeSpan _currentTime;
+        private DispatcherTimer _timerVideoTime;
 
-		public MainWindow()
-		{
-			InitializeComponent();
+        public MainWindow()
+        {
+            InitializeComponent();
+            ChangeIsPlaying(false);
+        }
 
-			DispatcherTimer timer = new DispatcherTimer();
-			timer.Interval = TimeSpan.FromSeconds(1);
-			timer.Tick += timer_Tick;
-			timer.Start();
-		}
+        private void Open_OnClick(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new()
+            {
+                Filter = @"Video files
+                   (*.mpg; *.mpeg; *.avi; *.mp4)| *.mpg; *.mpeg; *.avi; *.mp4"
+            };
+            if (openFileDialog.ShowDialog() == true)
+            {
+                mediaPlayer.Source = new Uri(openFileDialog.FileName);
+            }
+        }
 
-		private void timer_Tick(object sender, EventArgs e)
-		{
-			if ((mePlayer.Source != null) && (mePlayer.NaturalDuration.HasTimeSpan) && (!userIsDraggingSlider))
-			{
-				sliProgress.Minimum = 0;
-				sliProgress.Maximum = mePlayer.NaturalDuration.TimeSpan.TotalSeconds;
-				sliProgress.Value = mePlayer.Position.TotalSeconds;
-			}
-		}
+        private void About_OnClick(object sender, RoutedEventArgs e)
+        {
+        }
 
-		private void Open_CanExecute(object sender, CanExecuteRoutedEventArgs e)
-		{
-			e.CanExecute = true;
-		}
+        private void OnMediaEnded()
+        {
+            mediaPlayer.Stop();
+            ChangeIsPlaying(false);
+        }
 
-		private void Open_Executed(object sender, ExecutedRoutedEventArgs e)
-		{
-			OpenFileDialog openFileDialog = new OpenFileDialog();
-			openFileDialog.Filter = "Media files (*.mp3;*.mpg;*.mpeg)|*.mp3;*.mpg;*.mpeg|All files (*.*)|*.*";
-			if (openFileDialog.ShowDialog() == true)
-				mePlayer.Source = new Uri(openFileDialog.FileName);
-		}
+        private void ChangeIsPlaying(bool isPlaying)
+        {
+            _isplaying = isPlaying;
 
-		private void Play_CanExecute(object sender, CanExecuteRoutedEventArgs e)
-		{
-			e.CanExecute = (mePlayer != null) && (mePlayer.Source != null);
-		}
+            if (_isplaying)
+            {
+                btnPlayPause.Content = "Pause";
+                btnPlayPause.ToolTip = "Play the media";
+                mediaPlayer.Play();
+            }
+            else
+            {
+                btnPlayPause.Content = "Play"; 
+                btnPlayPause.Content = "Pause the media";
+                mediaPlayer.Pause();
+            }
+        }
 
-		private void Play_Executed(object sender, ExecutedRoutedEventArgs e)
-		{
-			mePlayer.Play();
-			mediaPlayerIsPlaying = true;
-		}
+        private void MediaElement_OnMediaEnded(object sender, RoutedEventArgs e)
+        {
+            OnMediaEnded();
+        }
 
-		private void Pause_CanExecute(object sender, CanExecuteRoutedEventArgs e)
-		{
-			e.CanExecute = mediaPlayerIsPlaying;
-		}
+        private void MediaElement_OnMediaFailed(object sender, ExceptionRoutedEventArgs e)
+        {
+            OnMediaEnded();
+        }
 
-		private void Pause_Executed(object sender, ExecutedRoutedEventArgs e)
-		{
-			mePlayer.Pause();
-		}
+        private void CloseWindow(object sender, RoutedEventArgs e)
+        {
+            mediaPlayer.Close();
+        }
+        
+        private void PlayPauseClicked(object sender, RoutedEventArgs e)
+        {
+            ChangeIsPlaying(!_isplaying);
+        }
 
-		private void Stop_CanExecute(object sender, CanExecuteRoutedEventArgs e)
-		{
-			e.CanExecute = mediaPlayerIsPlaying;
-		}
+        private void MyMediaElement_MediaOpened(object sender, RoutedEventArgs e)
+        {
+            _isNewFileOpened = true;
+            ChangeIsPlaying(true);
+            btnPlayPause.IsEnabled = true;
 
-		private void Stop_Executed(object sender, ExecutedRoutedEventArgs e)
-		{
-			mePlayer.Stop();
-			mediaPlayerIsPlaying = false;
-		}
+            // Create a timer that will update the counters and the time slider
+            _timerVideoTime = new()
+            {
+                Interval = TimeSpan.FromSeconds(1)
+            };
+            _timerVideoTime.Tick += new EventHandler(Timer_Tick);
+            _timerVideoTime.Start();
+        }
 
-		private void sliProgress_DragStarted(object sender, DragStartedEventArgs e)
-		{
-			userIsDraggingSlider = true;
-		}
-
-		private void sliProgress_DragCompleted(object sender, DragCompletedEventArgs e)
-		{
-			userIsDraggingSlider = false;
-			mePlayer.Position = TimeSpan.FromSeconds(sliProgress.Value);
-		}
-
-		private void sliProgress_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
-		{
-			lblProgressStatus.Text = TimeSpan.FromSeconds(sliProgress.Value).ToString(@"hh\:mm\:ss");
-		}
-
-		private void Grid_MouseWheel(object sender, MouseWheelEventArgs e)
-		{
-			mePlayer.Volume += (e.Delta > 0) ? 0.1 : -0.1;
-		}
-
-	}
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            // Check if the movie finished calculate it's total time
+            if (mediaPlayer.NaturalDuration.TimeSpan.TotalSeconds > 0)
+            {
+                if (_isNewFileOpened)
+                {
+                    _isNewFileOpened = false;
+                    playBackSlider.Maximum = mediaPlayer.NaturalDuration.TimeSpan.TotalSeconds;
+                    _currentTime = mediaPlayer.NaturalDuration.TimeSpan;
+                    totalTime.Text = mediaPlayer.NaturalDuration.TimeSpan.ToString(TIME_FORMAT);
+                }
+                if (_currentTime.TotalSeconds > 0)
+                {
+                    // Updating time slider
+                    playBackSlider.Value = mediaPlayer.Position.TotalSeconds;
+                    currentTime.Text = mediaPlayer.Position.ToString(TIME_FORMAT);
+                }
+            }
+        }
+    }
 }
